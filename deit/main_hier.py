ec92c0693250fe6cf2c83b21419c4e6e8979d301
gianghuong13 <nhg130904@gmail.com>
2026-02-07 17:16:14 +0700

    fix import module

--- deit/main_hier.py
+++ deit/main_hier.py
@@ -18,7 +18,7 @@ from timm.scheduler import create_scheduler
 from timm.optim import create_optimizer
 from timm.utils import NativeScaler, get_state_dict, ModelEma
 
-from datasets import build_dataset
+from dataset.datasets_custom import build_dataset
 
 from engine_vit_hier import train_one_epoch, evaluate
 from engine_vit_hier_eval import evaluate_detail
